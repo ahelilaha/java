6c8aff1acedcb5c7049af08ec434e676a39c6cf8
ASHIS LAHA <ashislaha@ASHISs-Personal-MacBook-Pro.local>
2024-12-23 15:38:57 -0800

    If a dog is barking then get up

---
+++ BarkingDog/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Main.shouldWakeUp(true, 11);
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+
+        if (barking == false) {
+            // If Dog isn't barking at all!
+            return false;
+
+        } else if (hourOfDay < 0 || hourOfDay > 23) {
+            // error case
+            return false;
+
+        } else if(hourOfDay < 8 || hourOfDay > 22) {
+            return true;
+
+        } else {
+            return false;
+        }
+
+    }
+}
