a8cab5a2bbfff5afce83e9aa97e5b4b5fc8aee16
ASHIS LAHA <ashislaha@ASHISs-Personal-MacBook-Pro.local>
2024-12-26 11:02:52 -0800

    Learning more about classes, constructors, override and modifiers

--- ClassExplorations/src/Main.java
+++ ClassExplorations/src/Main.java
@@ -1,21 +1,56 @@
 public class Main {
-    String myName = "Aheli";
 
     public static void main(String[] args) {
+        BabyChair aheliStudyChair = new BabyChair("black", 10, 20, true);
+        aheliStudyChair.isSwingable = true;
+        System.out.println(aheliStudyChair.descibeMe());
 
-        Main object1 = new Main();
-        Main object2 = new Main();
-        object2.myName = "Ashis";
+        Chair ashisDiningChair = new Chair("black", 5, 15, false);
+        System.out.println(ashisDiningChair.descibeMe());
 
-        object2.printMe();
-        object1.printMe();
+        ashisDiningChair = aheliStudyChair;
+        System.out.println(ashisDiningChair.descibeMe());
+        System.out.println(aheliStudyChair.descibeMe());
+        ashisDiningChair.cost = 8;
+        System.out.println(aheliStudyChair.cost);
+
+    }
+}
+
+class Chair {
+    // properties / attributes
+    String color;
+    double cost;
+    int height;
+    boolean isAdjustableHeight;
+
+    // constructor (object is allocated in memory using new)
+    public Chair(String color, double chairCost, int height, boolean isAdjustableHeight) {
+        this.color = color;
+        this.cost = chairCost;
+        this.height = height;
+        this.isAdjustableHeight = isAdjustableHeight;
     }
 
-    private void printMe() {
-        System.out.println("My name is "+ myName);
+    // methods / behaviors
+    public String descibeMe() {
+        return "I am a " + color + " color chair. " + "I am worth of $" + myCost() + " cost. My height is " + height + " cm. " + "Can you adjust my height? --> " + isAdjustableHeight;
     }
 
-    public void printOthers() {
-        System.out.println("My name is "+ myName);
+    private double myCost() {
+        return cost;
+    }
+}
+
+class BabyChair extends Chair {
+    boolean isSwingable;
+
+    public BabyChair(String color, double chairCost, int height, boolean isAdjustableHeight) {
+        super(color, chairCost, height, isAdjustableHeight);
+    }
+
+    @Override
+    public String descibeMe() {
+        return super.descibeMe() + " Am I swingable? --> " + isSwingable;
     }
 }
