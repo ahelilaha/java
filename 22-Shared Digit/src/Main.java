3dc716ed389a618663060a0f077cccfb4388c79e
ASHIS LAHA <ashislaha@ASHISs-Personal-MacBook-Pro.local>
2024-12-31 11:13:54 -0800

    Making a code that can find any common digit in a number and exploring more on set

--- 22-Shared Digit/src/Main.java
+++ 22-Shared Digit/src/Main.java
@@ -1,10 +1,39 @@
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-        boolean isSharedDigit = Main.hasSharedDigit(19, 99);
+        boolean isSharedDigit = Main.findOutCommonDigits(123456, 7890);
+        String exploringSet = Main.friendsList();
         System.out.println(isSharedDigit);
+        System.out.println(exploringSet);
     }
 
-    public static boolean hasSharedDigit(int num1, int num2){
+    public static boolean findOutCommonDigits(Integer a, Integer b) {
+        // 1. iterate through each number, store each digit into a set
+        // 2. compare two sets and find out intersection
+        // 3. if intersection result is not an empty set then return true, else false
+        HashSet<Integer> aDigitsSet = new HashSet<Integer>();
+        HashSet<Integer> bDigitsSet = new HashSet<Integer>();
+
+        while(a > 0) {
+            Integer remainder = a % 10;
+            a = a / 10;
+            aDigitsSet.add(remainder);
+        }
+        while(b > 0) {
+            Integer remainder = b % 10;
+            b = b / 10;
+            bDigitsSet.add(remainder);
+        }
+        System.out.println(aDigitsSet);
+        System.out.println(bDigitsSet);
+
+        // Now aDigitsSet contains intersected digits
+        aDigitsSet.retainAll(bDigitsSet);
+        return !aDigitsSet.isEmpty();
+    }
+
+    public static boolean hasSharedDigitForTwoDigitNumbers(int num1, int num2){
         if(num1 < 10 || num1 > 99 || num2 < 10 || num2 > 99){
             return false;
         }
@@ -15,11 +44,26 @@ public class Main {
         int num2FirstDigit = num2 / 10;
         int num2LastDigit = num2 % 10;
 
-        if((num1FirstDigit == num2FirstDigit) || (num1FirstDigit == num2LastDigit) || (num1LastDigit == num2FirstDigit) || (num1LastDigit == num2LastDigit)){
-            return true;
-        }else {
-            return false;
-        }
+        return (num1FirstDigit == num2FirstDigit) ||
+                (num1FirstDigit == num2LastDigit) ||
+                (num1LastDigit == num2FirstDigit) ||
+                (num1LastDigit == num2LastDigit);
+    }
+
+    public static String friendsList (){
+        HashSet<String> friends = new HashSet<String>();
+        friends.add("Diya");
+        System.out.println(friends);
+        friends.add("Navya");
+        System.out.println(friends);
+        friends.add("Rebeca");
+        System.out.println(friends);
+        friends.remove("Diya");
+        System.out.println(friends);
+        friends.contains("Diya");
+        friends.clear();
+        System.out.println(friends);
+        return "It worked!!!";
 
     }
 }
