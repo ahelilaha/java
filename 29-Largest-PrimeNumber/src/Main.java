910ce0f15e25b85052d02e9649daec8c93ac4a3c
ASHIS LAHA <ashislaha@ASHISs-Personal-MacBook-Pro.local>
2025-01-05 20:57:35 -0800

    Learning more about arrayList and how to get the largest prime number from an array

--- 29-Largest-PrimeNumber/src/Main.java
+++ 29-Largest-PrimeNumber/src/Main.java
@@ -4,23 +4,29 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> factors = findingFactors(12);
         // for-each
-        /*
         for(Integer eachFactor : factors){
-           if (Main.isPrimeNumber(eachFactor)){
-               System.out.println("largest prime factor "+eachFactor);
-                break;
-           }
+            System.out.println(eachFactor);
         }
-        */
 
-        // for-index
-        for(int i = 0; i < factors.size(); i++) {
+
+        // for-index for reverse iterating
+        int i = factors.size() - 1;
+        for( ; i >= 0; i--) {
             Integer factor = factors.get(i);
             if(Main.isPrimeNumber(factor)){
                 System.out.println("largest prime factor "+ factor);
                 break;
             }
         }
+
+        // for-index
+//        for(int i = 0; i < factors.size(); i++) {
+//            Integer factor = factors.get(i);
+//            if(Main.isPrimeNumber(factor)){
+//                System.out.println("largest prime factor "+ factor);
+//                break;
+//            }
+//        }
     }
 
     public static ArrayList<Integer> findingFactors(int number) {
@@ -30,7 +36,7 @@ public class Main {
             return factors; // an empty array
         }
 
-        for (int i = number - 1; i > 0; i--) {
+        for (int i = 1; i < number-1; i++) {
             if (number % i == 0) {
                 factors.add(i);
             }
